6760cf686624c141056d93e8feea1e8bacb5399a
Sushans Londhe <ballylondhe20@gmail.com>
2024-01-07 13:51:48 +0530

    updated Reviews

--- src/Components/ReviewCard.jsx
+++ src/Components/ReviewCard.jsx
@@ -9,7 +9,7 @@ export default function ReviewCard({ imgURL, customerName, rating, feedback }) {
         className="rounded-full  w-[120px] h-[120px] mt-10"
       />
       <p className=" mt-6 pl-10 max-w-sm info-text">{feedback}</p>
-      <div className=" mt-[10rem]   absolute  px-[8rem] flex justify-center items-center gap-2">
+      <div className=" absolute mt-[10rem]  max-sm:ml-[8rem] max-sm:px-[3rem] px-[8rem] flex justify-center items-center">
         <img
           src={star}
           width={24}
@@ -18,8 +18,8 @@ export default function ReviewCard({ imgURL, customerName, rating, feedback }) {
           className="object-contain m-0"
         />
         <p className="text-xl font-montserrat text-slate-gray">{rating}</p>
+        <h3 className=" font-palanquin text-right">{customerName}</h3>
       </div>
-      <h3 className=" font-palanquin text-right">{customerName}</h3>
     </div>
   );
 }
